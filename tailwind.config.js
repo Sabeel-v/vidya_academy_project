538918b8f8e4330246ea15e9445b1e1df316e7f2
Sabeel-v <sabeelv825@gmail.com>
2025-11-05 23:37:11 +0530

    last new ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx,html}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: [
+    "./index.html",
+    "./**/*.{html,js,ts,jsx,tsx}",   // catches your section file too
+  ],
+  theme: { extend: {} },
+  plugins: [],
+  // Optional: if you generate classes dynamically
+  // safelist: ["left-[300px]", "translate-x-[15%]"],
+}
