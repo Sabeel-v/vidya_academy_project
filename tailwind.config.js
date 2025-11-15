ac93b167957798f9032c974567e2425c7cd12672
Sabeel-v <sabeelv825@gmail.com>
2025-11-15 15:37:02 +0530

    mac work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./**/*.{html,js,ts,jsx,tsx}",   // catches your section file too
+    "./pages/**/*.{html,js}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
-  // Optional: if you generate classes dynamically
-  // safelist: ["left-[300px]", "translate-x-[15%]"],
 }
